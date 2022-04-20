5c63f662124acc17cc7369d89a2d4bff55689a83
Inácio Oliveira <inacioigne@gmail.com>
2022-04-20 12:55:12 -0400

    Update de exemplares

--- src/components/display_item/exemplares.js
+++ src/components/display_item/exemplares.js
@@ -9,9 +9,11 @@ import {
 } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { api } from "src/services/api";
-import { useContext, useRef, useState } from "react";
+import { useContext, useRef, useState, useCallback } from "react";
 import { ItemContext } from "src/admin/contexts/itemContext";
 import DeleteIcon from "@mui/icons-material/Delete";
+
+
 
 export default function Exemplares(props) {
   const {
@@ -21,16 +23,13 @@ export default function Exemplares(props) {
     checkboxExemplares,
     setCheckboxExemplares,
   } = useContext(ItemContext);
+  console.log('EX: ', rowsEx)
   const [selectionModel, setSelectionModel] = useState([]);
 
   const handleDelete = () => {
     const newRow = rowsEx.filter((row) => {
       return !selectionModel.includes(row.id);
     });
-
-    // const dellRow = rowsEx.filter((row) => {
-    //   return selectionModel.includes(row.id);
-    // });
 
     const pr = new Promise((resolve, reject) => {
       setPromiseArguments({ resolve, reject, newRow, selectionModel });
@@ -109,22 +108,123 @@ export default function Exemplares(props) {
       </Dialog>
     );
   };
+  {/** UPDATE EXEMPLAR */}
+  
+  // const processRowUpdate = useCallback(
+
+  //   (newRow, oldRow) =>
+    
+  //     new Promise((resolve, reject) => {
+  //       console.log('UP: ', newRow, oldRow)
+  //       const mutation = computeMutation(newRow, oldRow);
+  //       if (mutation) {
+          
+  //         setPromiseArguments({ resolve, reject, newRow, oldRow });
+         
+  //       } else {
+  //         resolve(oldRow);
+  //       }
+  //     }),
+  //   []
+  // );
+  const handleNoUpdate = () => {
+    const { oldRow, resolve } = promiseUpdateEx;
+    resolve(oldRow);
+ 
+    setPromiseUpdateEx(null);
+
+  };
+  const handleYesUpdate = async () => {
+    const { newRow, oldRow, reject, resolve } = promiseUpdateEx;
+    try {
+      
+
+     
+      console.log("NEWROW: ", newRow)
+
+    // patchData(data).catch((err) => {
+    //  console.error("ops! ocorreu um erro" + err);
+    //  });
+    //   const response = await mutateRow(newRow);
+    //   //console.log("RES:", response);
+    //   setSnackbar({ children: "Item atualizado com sucesso", severity: "success" });
+    //   resolve(response);
+      
+      
+      setPromiseArguments(null);
+    } catch (error) {
+      setSnackbar({ children: "Name can't be empty", severity: "error" });
+      reject(oldRow);
+      setPromiseArguments(null);
+    }
+  };
+
+  const renderConfirmUpdate = () => {
+    if (!promiseUpdateEx) {
+      return null;
+    }
+    const { newRow, oldRow } = promiseUpdateEx;
+    const mutation = computeMutation(newRow, oldRow);
+    
+    return (
+      <Dialog
+        maxWidth="xs"
+        TransitionProps={{ onEntered: handleEntered }}
+        open={!!promiseUpdateEx}
+      >
+        <DialogTitle>Tem certeza?</DialogTitle>
+        <DialogContent dividers>
+          {`Deseja substituir os ${mutation}.`}
+        </DialogContent>
+        <DialogActions>
+          <Button ref={noButtonRef} onClick={handleNoUpdate}>
+            Não
+          </Button>
+          <Button onClick={handleYesUpdate}>Sim</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+  function computeMutation(newRow, oldRow) {
+    if (newRow !== oldRow) {
+      //console.log('MUT :', oldRow.Registro)
+      return `Exemplar '${oldRow.Registro}'`;
+    }
+    return null;
+  }
+  const [promiseUpdateEx, setPromiseUpdateEx] = useState(null);
+  const updateEx = useCallback(
+    (newRow, oldRow) =>
+    new Promise((resolve, reject) => {
+      
+      const mutation = computeMutation(newRow, oldRow);
+      if (mutation) {
+        setPromiseUpdateEx({ resolve, reject, newRow, oldRow });
+        console.log('UP: ', promiseUpdateEx)
+
+      } else {
+                 resolve(oldRow);
+              }
+
+
+    }), [])
+  
 
   return (
     <Box>
       {renderConfirmDialog()}
+      {renderConfirmUpdate()}
 
       <Box style={{ height: 250, width: "100%" }}>
         <DataGrid
           columns={[
-            { field: "Biblioteca" },
-            { field: "Localização" },
-            { field: "Chamada" },
-            { field: "Localização" },
-            { field: "Volume" },
-            { field: "Exemplar" },
+            { field: "Biblioteca", width: 200, editable: true },
+            { field: "Localização", width: 150, editable: true },
+            { field: "Chamada", width: 150, editable: true },
+            { field: "Volume", editable: true },
+            { field: "Exemplar", editable: true },
             { field: "Registro" },
-            { field: "Status" },
+            { field: "Status", editable: true },
           ]}
           rows={rowsEx}
           hideFooter
@@ -133,6 +233,9 @@ export default function Exemplares(props) {
             setSelectionModel(newSelectionModel);
           }}
           selectionModel={selectionModel}
+          experimentalFeatures={{ newEditingApi: true }}
+          processRowUpdate={updateEx}
+       
         />
       </Box>
       <Button
