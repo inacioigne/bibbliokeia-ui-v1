3c5b5952868c9a4bbe3f536795f27d631740b333
Inácio Oliveira <inacioigne@gmail.com>
2022-04-28 07:45:09 -0400

    Forms

--- src/components/forms/datafield.js
+++ src/components/forms/datafield.js
@@ -6,90 +6,188 @@ import {
   MenuItem,
   Typography,
   Box,
+  ButtonGroup,
+  Button,
 } from "@mui/material/";
 import { ExpandMore } from "@mui/icons-material/";
-import { useForm, useFieldArray, Controller } from "react-hook-form";
+import { Controller } from "react-hook-form";
+import Indicators from "src/components/forms/indicators";
+import { Add, Remove } from "@mui/icons-material";
 
 export default function Datafield(props) {
-  return (
-    <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMore />}
-        aria-controls="content"
-        id="header"
-        sx={{ borderBottom: 1 }}
-      >
-        <Typography variant="h7" component="div" gutterBottom>
-          ISBN
-        </Typography>
-      </AccordionSummary>
-      <Box sx={{ p: 3 }}>
-        <Box
-          sx={{
-            display: "flex",
-            gap: 2,
-          }}
-        >
-          <Box>
-            {/**INDICADORES */}
-            <Box style={{ width: 215 }}>
-              <Controller
-                name={`datafields[${props.tag}].indicators.Ind1`}
-                control={props.control}
-                defaultValue={`datafields[${props.tag}].indicators.Ind1`}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    label="Ind1"
-                    variant="outlined"
-                    size="small"
-                    sx={{ width: 170 }}
-                  />
-                )}
-              />
-              <Controller
-                name={`datafields[${props.tag}].indicators.Ind2`}
-                control={props.control}
-                defaultValue={`datafields[${props.tag}].indicators.Ind2`}
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    label="Ind1"
-                    variant="outlined"
-                    size="small"
-                    sx={{ width: 170 }}
-                  />
-                )}
-              />
+  if (props.metadata.repeatable) {
+    return (
+      <Box>
+        {props.fields.map((field, index) => (
+          <Box
+            key={index}
+            sx={{
+              display: "flex",
+            }}
+          >
+            <Box style={{ flexGrow: 1 }}>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMore />}
+                  aria-controls="content"
+                  id="header"
+                  sx={{ borderBottom: 1 }}
+                >
+                  <Typography variant="h7" component="div" gutterBottom>
+                    {props.metadata.description}
+                  </Typography>
+                </AccordionSummary>
+                <Box sx={{ p: 3 }}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      gap: 2,
+                    }}
+                  >
+                    {/**INDICADORES */}
+                    <Box>
+                      <Box style={{ width: 215 }}>
+                        <Indicators
+                          metadata={props.metadata.indicators.Ind1}
+                          control={props.control}
+                          label="Ind1"
+                          name={`datafields[${props.metadata.tag}][${index}].indicators.Ind1`}
+                        />
+
+                        <Indicators
+                          metadata={props.metadata.indicators.Ind2}
+                          control={props.control}
+                          label="Ind2"
+                          name={`datafields[${props.metadata.tag}][${index}].indicators.Ind2`}
+                        />
+                      </Box>
+                    </Box>
+                    {/** SUBFIELDS */}
+                    <Box
+                      sx={{
+                        display: "grid",
+                        columnGap: 3,
+                        rowGap: 2,
+                        gridTemplateColumns: "repeat(2, 1fr)",
+                        mb: 4,
+                      }}
+                      style={{ width: "100%" }}
+                    >
+                      {props.metadata.subfields.map((subfield, subindex) => (
+                        <Controller
+                          key={subindex}
+                          name={`datafields[${props.metadata.tag}][${index}].subfields.${subfield.value}`}
+                          control={props.control}
+                          defaultValue=""
+                          render={({ field }) => (
+                            <TextField
+                              {...field}
+                              label={subfield.label}
+                              variant="outlined"
+                              size="small"
+                              sx={{ width: 170 }}
+                            />
+                          )}
+                        />
+                      ))}
+                    </Box>
+                  </Box>
+                </Box>
+              </Accordion>
             </Box>
+            <ButtonGroup variant="outlined" aria-label="outlined button group">
+              <Button onClick={() => props.append({})}>
+                <Add color="primary" />
+              </Button>
+              <Button onClick={() => props.remove(index)}>
+                <Remove color="primary" />
+              </Button>
+            </ButtonGroup>
+            {/*          
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                border: 1
+              }}
+            >
+              <Add color="primary" onClick={() => props.append({})} />
+            </Box> */}
           </Box>
+        ))}
+      </Box>
+    );
+  } else {
+    return (
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMore />}
+          aria-controls="content"
+          id="header"
+          sx={{ borderBottom: 1 }}
+        >
+          <Typography variant="h7" component="div" gutterBottom>
+            {props.metadata.description}
+          </Typography>
+        </AccordionSummary>
+        <Box sx={{ p: 3 }}>
           <Box
             sx={{
-              display: "grid",
-              columnGap: 3,
-              rowGap: 2,
-              gridTemplateColumns: "repeat(2, 1fr)",
-              mb: 4,
+              display: "flex",
+              gap: 2,
             }}
-            style={{ width: "100%" }}
           >
-            <Controller
-              name={`datafields[${props.tag}].subfields.a`}
-              control={props.control}
-              defaultValue={`datafields[${props.tag}].subfields.a`}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  label="ISBN"
-                  variant="outlined"
-                  size="small"
-                  sx={{ width: 170 }}
+            {/**INDICADORES */}
+            <Box>
+              <Box style={{ width: 215 }}>
+                <Indicators
+                  metadata={props.metadata.indicators.Ind1}
+                  control={props.control}
+                  label="Ind1"
+                  name={`datafields[${props.metadata.tag}].indicators.Ind1`}
                 />
-              )}
-            />
+
+                <Indicators
+                  metadata={props.metadata.indicators.Ind1}
+                  control={props.control}
+                  label="Ind2"
+                  name={`datafields[${props.metadata.tag}].indicators.Ind2`}
+                />
+              </Box>
+            </Box>
+            {/** SUBFIELDS */}
+            <Box
+              sx={{
+                display: "grid",
+                columnGap: 3,
+                rowGap: 2,
+                gridTemplateColumns: "repeat(2, 1fr)",
+                mb: 4,
+              }}
+              style={{ width: "100%" }}
+            >
+              {props.metadata.subfields.map((subfield, index) => (
+                <Controller
+                  key={index}
+                  name={`datafields[${props.metadata.tag}].subfields.${subfield.value}`}
+                  control={props.control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <TextField
+                      {...field}
+                      label={subfield.label}
+                      variant="outlined"
+                      size="small"
+                      sx={{ width: 170 }}
+                    />
+                  )}
+                />
+              ))}
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Accordion>
-  );
+      </Accordion>
+    );
+  }
 }
